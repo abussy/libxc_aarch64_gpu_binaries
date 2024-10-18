75eaef2f48fda7a1395fdd7f86356847198907e5
Augustin Bussy <augustin.bussy@cscs.ch>
2024-10-18 11:33:44 +0200

    Initial test binary for v7.0.0

--- build/cuda-11.4.3/build_script/build.sh
+++ build/cuda-11.4.3/build_script/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-version="6.2.2"
+version="7.0.0"
 url="https://gitlab.com/libxc/libxc/-/archive/${version}/libxc-${version}.tar.gz"
-expected_shasum="d1b65ef74615a1e539d87a0e6662f04baf3a2316706b4e2e686da3193b26b20f"
+expected_shasum="8d4e343041c9cd869833822f57744872076ae709a613c118d70605539fb13a77"
 
 wget $url
 
@@ -15,8 +15,8 @@ fi
 tar --extract --file libxc-${version}.tar.gz
 cd libxc-${version}
 
-patch -p1 CMakeLists.txt ../patches/cmake-cuda.patch
-patch -p1 src/util.h ../patches/source-fixes.patch
+patch -p1 < ../patches/cmake-cuda.patch
+patch -p1 < ../patches/source-fixes.patch
 
 mkdir libxc_build
 cd libxc_build
@@ -28,7 +28,9 @@ cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_XHOST=OFF -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_C_FLAGS="-I/opt/glibc-2.28/include" \
       -DCMAKE_EXE_LINKER_FLAGS="-L/opt/glibc-2.28/lib -Wl,--rpath=/opt/glibc-2.28/lib" \
       -DCMAKE_CUDA_FLAGS="-I/opt/glibc-2.28/include -L/opt/glibc-2.28/lib" \
+      -DCMAKE_SHARED_LINKER_FLAGS="-Wl,--disable-new-dtags" \
+      -DCMAKE_SKIP_RPATH=OFF \
       -DDISABLE_KXC=ON ..
 
-make -j 8
+make -j 4
 make install
